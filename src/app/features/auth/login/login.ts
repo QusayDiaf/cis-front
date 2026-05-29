316b66383e06e48521d2d5e0fa129c1dd1cc0825
qusay_diaf <kosayking15@gmail.com>
2026-05-29 08:37:24 +0200

    finish the login logic & regex

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -20,30 +20,44 @@ export class Login {
 
   // depindency injection
   constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
+  this.loginForm = this.fb.group({
+    email: ['', 
+      {
+        validators: [
+          Validators.required,
+          Validators.email
+        ],
+        updateOn: 'submit' 
+     }
+    ],
+
+    
+    password: [
+      '', 
+      {
+        validators: [
           Validators.required,
           Validators.minLength(6),
-          Validators.pattern(
-            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$',
-          ),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$')
         ],
-      ],
-    });
-  }
+        updateOn: 'submit' 
+      }
+    ],
+  });
+}
 
   onsubmit() {
+    console.log("the form is submitted");
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
+      console.log(this.loginForm.value ,"the form value");
     } else {
+      console.log("the form is invalid");
       this.loginForm.markAllAsTouched();
     }
   }
 
   rememberme: boolean = false;
+  
   remembermechange() {
     this.rememberme = !this.rememberme;
     console.log(this.rememberme);
