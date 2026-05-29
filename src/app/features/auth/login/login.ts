ff9591f4936e709b61272e5a0a5c30be0f476710
qusay_diaf <kosayking15@gmail.com>
2026-05-29 03:55:42 +0200

    working in submit problame

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,17 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm!: FormGroup;
 
-  hidepassword:boolean =true;
+  // depindency injection
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$',
+          ),
+        ],
+      ],
+    });
+  }
 
-  viewpassword(){
+  onsubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  rememberme: boolean = false;
+  remembermechange() {
+    this.rememberme = !this.rememberme;
+    console.log(this.rememberme);
+  }
+
+  hidepassword: boolean = true;
+
+  viewpassword() {
     this.hidepassword = !this.hidepassword;
   }
 }
