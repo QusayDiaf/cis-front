6574a4d45227b32be9b49b6b1bcb3f1fccc9f2db
qusay_diaf <kosayking15@gmail.com>
2026-05-30 21:41:03 +0200

    add api services

--- src/app/services/api.spec.ts
+++ src/app/services/api.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { User } from './user';
+import { Api } from './api';
 
-describe('User', () => {
-  let service: User;
+describe('Api', () => {
+  let service: Api;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(User);
+    service = TestBed.inject(Api);
   });
 
   it('should be created', () => {
